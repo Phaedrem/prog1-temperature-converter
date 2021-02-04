8eff1aab9875f9868b0d02a008482d0dd7402834
Darren Bowers <darren.bowers@g.austincc.edu>
2021-02-04 13:31:37 -0600

    created main.py file with proper header

---
+++ main.py
@@ -0,0 +1,6 @@
+######################
+# Name: Darren Bowers
+# Coding 03
+# Purpose: In this assignment we ask the user for multiple inputs, and make a decision and calculations based on that input.
+######################
+
