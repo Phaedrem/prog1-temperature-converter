bcae34246fccffcefa959b35bf5acd8e39ab30d0
Darren Bowers <darren.bowers@g.austincc.edu>
2021-02-07 12:48:26 -0600

    defined USER_CHOICE with input command to test functionality

--- main.py
+++ main.py
@@ -4,3 +4,5 @@
 # Purpose: In this assignment we ask the user for multiple inputs, and make a decision and calculations based on that input.
 ######################
 
+USER_CHOICE = input("Enter \n 1) Convert Fahrenheit to Celsius \n 2) Convert Celsius to Fahrenheit \n")
+print("Is", USER_CHOICE, "your final answer?")
